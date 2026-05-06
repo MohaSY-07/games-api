4642277623f9392eb4ce3c18a7ac59418dfaf878
MohaSY-07 <hirure07@gmail.com>
2026-05-06 10:55:50 +0200

    add POST route to create game with validation

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./data.db');
-const express = require('express');
-const app = express();
+    const express = require('express');
+    const app = express();
+
+    app.use(express.json());
+
+    const sqlite3 = require('sqlite3').verbose();
+    const db = new sqlite3.Database('./data.db');
 
 
-app.get('/', (req, res) => {
-   res.send('Hello, server is working!');
+
+    app.get('/', (req, res) => {
+    res.send('Hello, server is working!');
+    });
+
+
+
+    app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    });
+    // table
+    db.run(`
+    CREATE TABLE IF NOT EXISTS games (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        genre TEXT,
+        platform TEXT
+    )
+    `);
+
+
+app.post('/api/games', (req, res) => {
+    const { title, genre, platform } = req.body;
+
+    // validation
+    if (!title || !genre || !platform) {
+        return res.status(400).send('Missing fields');
+    }
+
+    const sql = `INSERT INTO games (title, genre, platform) VALUES (?, ?, ?)`;
+
+    db.run(sql, [title, genre, platform], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+
+        res.status(201).json({
+            id: this.lastID,
+            title,
+            genre,
+            platform
+        });
+    });
 });
-
-
-app.listen(3000, () => {
-   console.log('Server started on port 3000');
-});
-
-db.run(`
-CREATE TABLE IF NOT EXISTS games (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    genre TEXT,
-    platform TEXT
-)
-`);
-
